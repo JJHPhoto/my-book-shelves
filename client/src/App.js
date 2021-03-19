92e9a6396012ae1c9e70876d1c30faebfce08a6b
Joshua J. Haller <phone@joshuajhaller.com>
2021-03-19 16:33:58 -0700

    Forgot to save App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./main.sass";
 import GlobalNavbar from "./components/GlobalNavbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
